07e6491856685c7473c668d414e613dfb75bc2ee
FrancoMonteros <franco.n.monteros@gmail.com>
2023-01-23 00:19:13 -0300

    se termino el crud

--- js/admin.js
+++ js/admin.js
@@ -1,11 +1,22 @@
+class Productos {
+    constructor(producto, codigo, descripcion, precio, URL) {
+        this.producto = producto;
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.URL = URL;
+    }
+}
+
 let inputproducto = document.getElementById('inputproducto');
 let inputcodigo = document.getElementById('inputcodigo');
 let inputdescripcion = document.getElementById('inputdescripcion');
 let inputprecio = document.getElementById('inputprecio');
 let inputURL = document.getElementById('inputURL');
 let form = document.getElementById('form');
-import { Productos } from "../interfaces/producto.js";
+
 let arrPruducto = JSON.parse(localStorage.getItem('listaProductos')) || [];
+
 let tbodyListado = document.getElementById('tbodyListado');
 console.log(tbodyListado);
 
@@ -13,6 +24,10 @@ console.log(tbodyListado);
 
 const handleSubmit = (e) => {
     e.preventDefault();
+    if (inputproducto.value === "" || inputcodigo === "" || inputdescripcion === "" || inputprecio === "" || inputURL === "") {
+        alert("todos los campos son obligatorios")
+        return;
+    }
     const nuevoProducto = new Productos(
         inputproducto.value,
         inputcodigo.value,
@@ -21,26 +36,56 @@ const handleSubmit = (e) => {
         inputURL.value)
     arrPruducto.push(nuevoProducto);
     localStorage.setItem('listaProductos', JSON.stringify(arrPruducto));
-    form.reset();
-    listaProductos(nuevoProducto)
+    window.location.reload();
+    listaProductos()
 }
 form.addEventListener('submit', handleSubmit);
 
 
-const listaProductos = (producto) => {
-    tbodyListado.innerHTML += `
-    <tr>
-    <th>${producto.producto}</th>
-    <th>${producto.precio}</th>
-    <th>${producto.codigo}</th>
-    <th>${producto.descripcion}</th>
-    <th>${producto.URL}</th>
-    <th>
-        <button class="btn btn-danger">borrar</button>
-        <button class="btn btn-warning">editar</button>
-    </th>
-</tr>`
+const listaProductos = () => {
+    arrPruducto.forEach(producto => {
+        return tbodyListado.innerHTML += `
+            <tr>
+                <th>${producto.producto}</th>
+                <th>${producto.precio}</th>
+                <th>${producto.codigo}</th>
+                <th>${producto.descripcion}</th>
+                <th>${producto.URL}</th>
+                <th>
+                    <button onclick="borrarProducto(${producto.codigo})" class="btn btn-danger">borrar</button>
+                    <button onclick="editarProducto(${producto.codigo})" class="btn btn-warning">editar</button>
+                </th>
+            </tr>`
+    });
 }
-arrPruducto.forEach(element => {
-    listaProductos(element)
-});
+const borrarProducto = (codigo) => {
+    console.log(codigo);
+    const arrNuevoProducto = arrPruducto.filter(producto => {
+        return producto.codigo !== codigo.toString();
+    })
+    console.log(arrNuevoProducto);
+    arrPruducto = arrNuevoProducto;
+    localStorage.setItem('listaProductos', JSON.stringify(arrPruducto));
+    window.location.reload();
+}
+
+const editarProducto = (codigo) => {
+    console.log(codigo);
+    const producto = arrPruducto.find(producto => {
+        return producto.codigo === codigo.toString();
+    })
+    console.log(producto);
+    inputproducto.value = producto.producto,
+        inputcodigo.value = producto.codigo,
+        inputdescripcion.value = producto.descripcion,
+        inputprecio.value = producto.precio,
+        inputURL.value = producto.URL
+
+    const arrNuevoProducto = arrPruducto.filter(producto => {
+        return producto.codigo !== codigo.toString();
+    })
+    arrPruducto = arrNuevoProducto;
+    localStorage.setItem('listaProductos', JSON.stringify(arrPruducto));
+}
+
+listaProductos()
